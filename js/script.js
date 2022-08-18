f7e1c05abf438cc08df3b3a86c3cd0ed026ad2f6
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-08-19 00:08:10 +0200

    Add carousel to courses section

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let section = document.querySelector(".explain_");
 
 let courses = ["Python", "AWS", "Data Science", "Excel", "Web Development", "JavaScript", "Drawing"]
 
-let cur_course = "Python";
+let cur_course = "Python", n = 4;
 
 //getting data from api and storing it data variable
 function fetchdata(){
@@ -36,8 +36,20 @@ function build_description(course){
 
 // building courses
 function build_cards(course = "", search = undefined){
-    cards_container = document.createElement("section");
-    cards_container.className = "container_";
+    let cards_container = document.createElement("section");
+    cards_container.className = "container_ carousel slide";
+    cards_container.id = "carouselHeader";
+    cards_container.setAttribute("data-ride", "carousel");
+    cards_container.setAttribute("data-interval", "false");
+
+    let car_inner = document.createElement("div");
+    car_inner.className = "carousel-inner";
+    let f = true;
+    let carouselitem = document.createElement("div");
+    carouselitem.className = "carousel-item active";
+    let carddds = document.createElement("div")
+    carddds.className = "cards_"
+    let counter = 0;
     // building cards
     for(const item of courses){
         if(search != undefined) course = item;
@@ -49,75 +61,132 @@ function build_cards(course = "", search = undefined){
                     search = search.toLowerCase()
                     if(!title.includes(search)) continue;
                 }
-                
-                let instructor = "";
-                for(const instr in data.record[course].courses[item].instructors){
-                    instructor += data.record[course].courses[item].instructors[instr].name + ', ';
+
+                if(counter == n){
+                    carouselitem.appendChild(carddds);
+                    car_inner.appendChild(carouselitem);
+                    carouselitem = document.createElement("div");
+                    carouselitem.className = "carousel-item";
+                    carddds = document.createElement("div");
+                    carddds.className = "cards_";
+                    counter = 0;
                 }
-                instructor = instructor.slice(0, -2);
-
-                cardlink = document.createElement("a");
-                cardlink.href = "";
-                cards_container.className = "container_";
-                datacontainer = document.createElement("div");
-                datacontainer.className = "course_";
-
-                image = document.createElement("img");
-                image.src = data.record[course].courses[item].image;
-                image.alt = "course thumbnail";
-                image.className = "thumb_";
-                
-                heading = document.createElement("h6");
-                headingtxt = document.createTextNode(data.record[course].courses[item].title);
-                heading.appendChild(headingtxt);
-                
-                names = document.createElement("p")
-                namestxt = document.createTextNode(instructor);
-                names.appendChild(namestxt);
-
-                ratingcontainer = document.createElement("div");
-                ratingcontainer.className = "rating_";
-                
-                rate = document.createElement("span");     
-                ratetxt = document.createTextNode(data.record[course].courses[item].rating.toFixed(2) + " "); 
-                rate.appendChild(ratetxt);
-
-                star1 = document.createElement("span");
-                star1.className = "fa fa-star";
-                star2 = document.createElement("span");
-                star2.className = "fa fa-star";
-                star3 = document.createElement("span");
-                star3.className = "fa fa-star";
-                star4 = document.createElement("span");
-                star4.className = "fa fa-star";
-                star5 = document.createElement("span");
-                star5.className = "fa fa-star-half-stroke";
-                ratingcontainer.appendChild(rate);
-                ratingcontainer.appendChild(star1);
-                ratingcontainer.appendChild(star2);
-                ratingcontainer.appendChild(star3);
-                ratingcontainer.appendChild(star4);
-                ratingcontainer.appendChild(star5);
-
-                price = document.createElement("span")
-                pricetxt = document.createTextNode('$' + data.record[course].courses[item].price)
-                price.appendChild(pricetxt);
-
-                datacontainer.appendChild(image);
-                datacontainer.appendChild(heading);
-                datacontainer.appendChild(names);
-                datacontainer.appendChild(ratingcontainer);
-                datacontainer.appendChild(price);
-
-                cardlink.appendChild(datacontainer);
-
-
-                cards_container.appendChild(cardlink);
+                counter++;
+                let cardlink = build_card(course, item);
+                carddds.appendChild(cardlink);
             }
         }
     }
+    if(counter != 0){
+        carouselitem.appendChild(carddds);
+        car_inner.appendChild(carouselitem);
+    }
+    cards_container.appendChild(car_inner);
+    
+    let button = document.createElement("button");
+    button.className = "carousel-control-prev";
+    button.type = "button";
+    button.setAttribute("data-bs-target", "#carouselHeader");
+    button.setAttribute("data-bs-slide", "prev");
+
+    let span1 = document.createElement("span");
+    span1.className = "carousel-control-prev-icon";
+    span1.setAttribute("aria-hidden","true")
+    let span2 = document.createElement("span");
+    span2.className = "visually-hidden";
+    let spantxt = document.createTextNode("Previous");
+    span2.appendChild(spantxt);
+    button.appendChild(span1);
+    button.appendChild(span2);
+    cards_container.appendChild(button);
+
+
+    button = document.createElement("button");
+    button.className = "carousel-control-next";
+    button.type = "button";
+    button.setAttribute("data-bs-target", "#carouselHeader");
+    button.setAttribute("data-bs-slide", "next");
+
+    span1 = document.createElement("span");
+    span1.className = "carousel-control-next-icon";
+    span1.setAttribute("aria-hidden","true")
+    span2 = document.createElement("span");
+    span2.className = "visually-hidden";
+    spantxt = document.createTextNode("Next");
+    span2.appendChild(spantxt);
+    button.appendChild(span1);
+    button.appendChild(span2);
+    cards_container.appendChild(button);
+
     section.appendChild(cards_container);
 }
+
+
+function build_card(course, item){
+    let instructor = "";
+    for(const instr in data.record[course].courses[item].instructors){
+    instructor += data.record[course].courses[item].instructors[instr].name + ', ';
+    }
+    instructor = instructor.slice(0, -2);
+
+    let cardlink = document.createElement("a");
+    cardlink.href = "";
+    cardlink.className = "course-cards_";
+    let datacontainer = document.createElement("div");
+    datacontainer.className = "course_";
+
+    let image = document.createElement("img");
+    image.src = data.record[course].courses[item].image;
+    image.alt = "course thumbnail";
+    image.className = "thumb_";
+                
+    let heading = document.createElement("h6");
+    let headingtxt = document.createTextNode(data.record[course].courses[item].title);
+    heading.appendChild(headingtxt);
+                
+    let names = document.createElement("p")
+    let namestxt = document.createTextNode(instructor);
+    names.appendChild(namestxt);
+
+    let ratingcontainer = document.createElement("div");
+    ratingcontainer.className = "rating_";
+                
+    let rate = document.createElement("span");     
+    let ratetxt = document.createTextNode(data.record[course].courses[item].rating.toFixed(2) + " "); 
+    rate.appendChild(ratetxt);
+
+    let star1 = document.createElement("span");
+    star1.className = "fa fa-star";
+    let star2 = document.createElement("span");
+    star2.className = "fa fa-star";
+    let star3 = document.createElement("span");
+    star3.className = "fa fa-star";
+    let star4 = document.createElement("span");
+    star4.className = "fa fa-star";
+    let star5 = document.createElement("span");
+    star5.className = "fa fa-star-half-stroke";
+    ratingcontainer.appendChild(rate);
+    ratingcontainer.appendChild(star1);
+    ratingcontainer.appendChild(star2);
+    ratingcontainer.appendChild(star3);
+    ratingcontainer.appendChild(star4);
+    ratingcontainer.appendChild(star5);
+
+    let price = document.createElement("span")
+    let pricetxt = document.createTextNode('$' + data.record[course].courses[item].price)
+    price.appendChild(pricetxt);
+
+    datacontainer.appendChild(image);
+    datacontainer.appendChild(heading);
+    datacontainer.appendChild(names);
+    datacontainer.appendChild(ratingcontainer);
+    datacontainer.appendChild(price);
+
+    cardlink.appendChild(datacontainer);
+
+    return cardlink;
+}
+
 
 
 // change the courses when change the topic
@@ -151,5 +220,11 @@ document.forms[0].onsubmit = function(e){
     }
 }   
 
+
 // featch the data on loading 
 fetchdata();
+
+
+
+
+
