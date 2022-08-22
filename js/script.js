00237e7b0bc842c96c8520dffb69da795344e359
Mahmoud Aboelsoud <56567087+aboelsooud@users.noreply.github.com>
2022-08-22 13:29:45 +0200

    fix: spacing in script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 let data;
-
 // getting the section we need to change
-let section = document.querySelector(".explain");
-
-
-let courses = ["Python", "AWS", "Data Science", "Excel", "Web Development", "JavaScript", "Drawing"]
-
+const section = document.querySelector(".explain");
+const courses = ["Python", "AWS", "Data Science", "Excel", "Web Development", "JavaScript", "Drawing"]
 let cur_course = "Python";
 
 //getting data from api and storing it data variable
@@ -19,7 +15,6 @@ function fetchdata(){
     });
 }
 
-
 //build the description and the button above the courses
 function build_description(course){
     section.innerHTML = "";
@@ -29,10 +24,8 @@ function build_description(course){
         <a href=""><b>Explore ${course}</b></a>
     `
     section.innerHTML += description;
-
     build_cards(course);
 }
-
 
 // building courses
 function build_cards(course = "", search = undefined){
@@ -49,39 +42,31 @@ function build_cards(course = "", search = undefined){
                     search = search.toLowerCase()
                     if(!title.includes(search)) continue;
                 }
-                
                 let instructor = "";
                 for(const instr in data.record[course].courses[item].instructors){
                     instructor += data.record[course].courses[item].instructors[instr].name + ', ';
                 }
                 instructor = instructor.slice(0, -2);
-
                 cardlink = document.createElement("a");
                 cardlink.href = "";
                 cards_container.className = "container";
                 datacontainer = document.createElement("div");
                 datacontainer.className = "course";
-
                 image = document.createElement("img");
                 image.src = data.record[course].courses[item].image;
                 image.alt = "course thumbnail";
                 image.className = "thumb";
-                
                 heading = document.createElement("h4");
                 headingtxt = document.createTextNode(data.record[course].courses[item].title);
                 heading.appendChild(headingtxt);
-                
                 names = document.createElement("p")
                 namestxt = document.createTextNode(instructor);
                 names.appendChild(namestxt);
-
                 ratingcontainer = document.createElement("div");
                 ratingcontainer.className = "rating";
-                
                 rate = document.createElement("span");     
                 ratetxt = document.createTextNode(data.record[course].courses[item].rating.toFixed(2) + " "); 
                 rate.appendChild(ratetxt);
-
                 star1 = document.createElement("span");
                 star1.className = "fa fa-star";
                 star2 = document.createElement("span");
@@ -98,27 +83,21 @@ function build_cards(course = "", search = undefined){
                 ratingcontainer.appendChild(star3);
                 ratingcontainer.appendChild(star4);
                 ratingcontainer.appendChild(star5);
-
                 price = document.createElement("span")
                 pricetxt = document.createTextNode('$' + data.record[course].courses[item].price)
                 price.appendChild(pricetxt);
-
                 datacontainer.appendChild(image);
                 datacontainer.appendChild(heading);
                 datacontainer.appendChild(names);
                 datacontainer.appendChild(ratingcontainer);
                 datacontainer.appendChild(price);
-
                 cardlink.appendChild(datacontainer);
-
-
                 cards_container.appendChild(cardlink);
             }
         }
     }
     section.appendChild(cards_container);
 }
-
 
 // change the courses when change the topic
 for(const item in courses){
